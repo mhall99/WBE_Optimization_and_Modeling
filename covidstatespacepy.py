64dbf21f17f409c31e2ae35f8194ce153ea26a31
Christian Snowden <70285002+CXSnowden@users.noreply.github.com>
2021-03-24 11:28:15 -0500

    Add files via upload

--- covidstatespacepy.py
+++ covidstatespacepy.py
@@ -29,22 +29,48 @@ flowtime = []
 #***************************************************
 No = 1935 #initial viral load
 Nt=No
+#until we can get length from inp this will be a placeholder
+length=5000
+n = length/100 #number of states in the system
+m = 2 #number of inputs
 
 #input output matrices which are constant for now. may change if we make
 #   state space time variant
-length = 5000 #swmm.get_from_input(inp, LINK, LENGTH)
-n = length/100 #number of states in the system
-m = 2 #number of inputs
 A = np.eye(n) #details decay factors of viral load for each state
 B = np.eye(m,n) #details location of viral inputs
 C = np.eye(n) #details location of sensors in the system (assuming all nodes have sensors)
-D = 0
+D=0
+
+
+
+#sample methodlolgy for updating A matrix
+
+#k represents the half-life/viral decay.
+#k=np.log(No-Nt)/flowtime
+
+#Nt = No**(k*flowtime)#update Nt to proper viral volume for next iteration
+
+#transpose to invert row and column for sake of signals needs
+#signal.StateSpace(A.T,B.T,C.T,D)
+
+#dx = A*x + B*u
+#y = C*x
+#def dx(t,x):
+        #u = 100
+        #dx = A*x + B*u
+    #xs = np.linspace(0,5,100)
+    #ys = odeint(dx, No, xs)
+    #tspan = np.linspace(0,1000)
+    #plt.plot(tspan, y)
+
 
 #str stores the string ids of all nodes found while id stores raw ids which can
 #   be used to find inflow and outflow of any give node
 allnodesstr=[]
 allnodesid=[]
 nodecount=0
+
+
 
 with pyswmm.Simulation(inp) as sim:
     links = pyswmm.Links(sim)
@@ -55,23 +81,22 @@ with pyswmm.Simulation(inp) as sim:
        
         allnodesid.append(nodes[node.nodeid])
         allnodesstr.append(node.nodeid)
-        nodecount= nodecount+1
+        nodecount=nodecount+1
         
     #steps through the simulation and allows for information to be gathered
     # at each step of the simulation
     for step in sim:
         count=0
+        
         #print the name of the node and the total inflow of it throughout the
-        #   simulation. can store the results in various arrays
+        #   simulation. can be used to store the results in various arrays
         while count <nodecount:
             print(allnodesstr[count], allnodesid[count].total_inflow)
             count=count+1
+            
 
 
-
-
-
-
-
-
+#python dict function to store all our list and big data. savable into a json.
+#django web server. has a data model which stores all componenets and measurements
+#   possibly use to organziealll data. django
 
